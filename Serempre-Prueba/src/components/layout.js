d6e2669772fa04177e905a9201010c61a899f249
Gongo <jvilla1714@gmail.com>
2020-09-29 15:02:32 -0500

    Added configuration for redux and redux persist

--- Serempre-Prueba/src/components/layout.js
+++ Serempre-Prueba/src/components/layout.js
@@ -9,6 +9,9 @@ import React from "react"
 import { AppBar, Toolbar, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { Link } from 'gatsby'
+import { Provider } from 'react-redux'
+import config from '../config/config'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const useStyles = makeStyles(theme=>({
   appBar:{
@@ -17,24 +20,28 @@ const useStyles = makeStyles(theme=>({
   }
 }))
 
+const {store, persistor} = config();
+
 const Layout = ({children}) =>{
   const classes = useStyles();
   return (
-    <>
-      <AppBar position='fixed' className={classes.appBar}>
-        <Toolbar>
-          <Button color='inherit'>
-            <Link style={{color:'white'}} to='/'>TODO</Link>
-          </Button>
-          <Button color='inherit'>
-            <Link style={{ color: 'white' }} to='/Map'>MAP</Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <div>
-        {children}
-      </div>
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppBar position='fixed' className={classes.appBar}>
+          <Toolbar>
+            <Button color='inherit'>
+              <Link style={{ color: 'white' }} to='/'>TODO</Link>
+            </Button>
+            <Button color='inherit'>
+              <Link style={{ color: 'white' }} to='/Map'>MAP</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <div>
+          {children}
+        </div>
+      </PersistGate>
+    </Provider>
   )
 }
 
